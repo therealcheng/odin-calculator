03232136cb1675192be7b29bd23b0f697e2763f9
Cheng Lim <wenyong.lim@icloud.com>
2022-10-10 19:28:40 +0100

    HTML structure and calculator functions.

---
+++ script.js
@@ -0,0 +1,35 @@
+const add = function (x, y) {
+  return x + y;
+};
+
+const subtract = function (x, y) {
+  return x - y;
+};
+
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
+
+const multiply = function (array) {
+  let sum = 1;
+  for (let i = 0; i < array.length; i++) {
+    sum *= array[i];
+  }
+  return sum;
+};
+
+const power = function (x, y) {
+  return x ** y;
+};
+
+const factorial = function (x) {
+  let sum = 1;
+  for (let i = 0; i < x; i++) {
+    if (i !== 0) sum *= i + 1;
+  }
+  return sum;
+};
