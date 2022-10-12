02ff4118a81de84dfa66c24c756fe74f5c8a2516
Cheng Lim <wenyong.lim@icloud.com>
2022-10-12 15:58:48 +0100

    Change numbers to null when clicking clear button

--- script.js
+++ script.js
@@ -13,16 +13,19 @@ let number1;
 let number2;
 let answer;
 
+// * Functions
+
 const add = function (x, y) {
   return Number(x) + Number(y);
 };
 
 const clearView = function () {
+  aboveView.textContent = '';
   view.textContent = '0';
   currentOperator = 0;
-  number1 = 0;
-  number2 = 0;
-  answer = 0;
+  number1 = null;
+  number2 = null;
+  answer = null;
   console.clear();
   console.log('cleared');
 };
@@ -37,19 +40,21 @@ const deleteView = function () {
   view.textContent = viewContent;
 };
 
+// * Event listeners
+
 operators.forEach((op) =>
   op.addEventListener('click', () => {
-    // * if an operator is clicked (add, subtract, divide, minus)
-
     if (op.textContent === '+') {
+      currentOperator = '+';
+
       number1 = view.textContent;
       console.log(number1);
     }
 
     if (op.textContent === '=') {
+      aboveView.textContent = ` ${number1} ${currentOperator} ${view.textContent} =`;
       currentOperator = '=';
       number2 = view.textContent;
-      aboveView.textContent = '';
       view.textContent = add(number1, number2);
       currentOperator = '';
       return;
